0ede4f75de583acda2ebc1ed5f91db251abd470b
freducus <a>
2022-07-29 14:46:38 +0200

    Tutorial 05

--- node_graphics_node.py
+++ node_graphics_node.py
@@ -63,7 +63,7 @@ class QDMGraphicsNode(QGraphicsItem):
         path_content.addRect(0, self.title_height, self.width, self.edge_size)
         painter.setPen(Qt.NoPen)
         painter.setBrush(self._brush_background)
-        painter.drawPath(path_title.simplified())
+        painter.drawPath(path_content.simplified())
 
 
         # outline
